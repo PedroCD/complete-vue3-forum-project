3151156b7a8aae03ed9934e454ccb0f182a89053
PedroCD <pedro@buyviu.com>
2023-09-18 15:04:25 +0100

    creating threadlist and cleaning pagehome a little

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,57 +1,15 @@
 <template>
-  <div v-for="thread in threads"
-    :key="thread.id"
-     class="col-large push-top"
-  >
-    <h1>{{ thread.title }}</h1>
-
-    <div class="post-list">
-      <div class="post"
-           v-for="postId in thread.posts"
-          :key="postId"
-      >
-
-        <div class="user-info">
-          <a href="#" class="user-name">{{userById(postById(postId).userId).name}}</a>
-
-          <a href="#">
-            <img :src="userById(postById(postId).userId).avatar" alt="" class="avatar-large">
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{posts.find(p => p.id === postId).text}}
-            </p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{postById(postId).publishedAt}}
-        </div>
-      </div>
-    </div>
-  </div>
+  <h1>Welcome to the forum</h1>
+  <ThreadList :threads="threads"/>
 </template>
 
 <script setup>
 import sourceData from '@/data.json'
+import ThreadList from '@/components/ThreadList.vue'
 import { ref } from 'vue'
 
 const threads = ref(sourceData.threads)
-const posts = ref(sourceData.posts)
-const users = ref(sourceData.users)
 
-function postById (postId) {
-  return posts.value.find(p => p.id === postId)
-}
-
-function userById (userId) {
-  return users.value.find(u => u.id === userId)
-}
 </script>
 
 <style>
---
+++ src/components/ThreadList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="col-full">
+
+    <div class="thread-list">
+
+      <h2 class="list-title">Threads</h2>
+
+      <div v-for="thread in threads" :key="thread.id" class="thread">
+        <div>
+          <p>
+            <a href="#">{{ thread.title }}</a>
+          </p>
+          <p class="text-faded text-xsmall">
+            By <a href="#">{{ userById(thread.userId).name }}</a>, {{ thread.publishedAt }}.
+          </p>
+        </div>
+
+        <div class="activity">
+          <p class="replies-count">
+            {{ thread.posts.length }} replies
+          </p>
+
+          <img class="avatar-medium" :src="userById(thread.userId).avatar" alt="">
+
+          <div>
+            <p class="text-xsmall">
+              <a href="#">{{ userById(thread.userId).name }}</a>
+            </p>
+            <p class="text-xsmall text-faded">{{ thread.publishedAt }}</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+import sourceData from '@/data.json'
+import { ref, defineProps } from 'vue'
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  threads: {
+    type: Array,
+    required: true
+  }
+})
+
+const posts = ref(sourceData.posts)
+const users = ref(sourceData.users)
+
+// eslint-disable-next-line no-unused-vars
+const postById = (postId) => {
+  return posts.value.find(p => p.id === postId)
+}
+
+const userById = (userId) => {
+  return users.value.find(p => p.id === userId)
+}
+</script>
+
+<style scoped>
+</style>
