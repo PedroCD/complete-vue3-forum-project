3dc1852bf956e978a635925f68a56b565ee62d3d
PedroCD <pedro@buyviu.com>
2023-09-18 15:43:58 +0100

    add redirect without changing url

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import PageHome from '@/components/PageHome.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import PageThreadShow from '@/components/PageThreadShow.vue'
 import PageNotFound from '@/components/PageNotFound.vue'
+import sourceData from '@/data.json'
 
 const routes = [
   {
@@ -13,7 +14,21 @@ const routes = [
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
-    props: true
+    props: true,
+    beforeEnter (to, from, next) {
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
