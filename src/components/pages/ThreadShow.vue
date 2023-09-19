b5b183774268cf89cccfc9882a18369964053f38
PedroCD <pedro@buyviu.com>
2023-09-19 10:45:18 +0100

    create post editor

--- src/components/pages/ThreadShow.vue
+++ src/components/pages/ThreadShow.vue
@@ -3,7 +3,16 @@
     <h1>{{ thread.title }}</h1>
 
     <post-list :posts="threadPosts" />
-
+    <div class="col-full">
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea v-model="newPostText" name="" id="" cols="30" rows="10" class="form-input" />
+        </div>
+        <div class="form-actions">
+          <button class="btn-blue">Submit post</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -14,6 +23,7 @@ import { computed, ref, defineProps } from 'vue'
 
 const threads = ref(sourceData.threads)
 const posts = ref(sourceData.posts)
+let newPostText = ref('')
 
 const props = defineProps({
   id: {
@@ -30,6 +40,19 @@ const threadPosts = computed(() => {
   return posts.value.filter(post => post.threadId === props.id)
 })
 
+const addPost = () => {
+  const postId = 'ggg' + Math.random()
+  const post = {
+    id: postId,
+    test: newPostText.value,
+    publishedAt: Math.floor(Date.now() / 1000),
+    threadId: props.id,
+    userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+  }
+  posts.value.push(post)
+  thread.value.posts.push(post)
+  newPostText = ''
+}
 </script>
 
 <style scoped>
