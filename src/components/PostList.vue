3c49f254043371e581cbc3608ffbff5ccb3a7835
PedroCD <pedro@buyviu.com>
2023-09-19 09:16:26 +0100

    creating postlist component

---
+++ src/components/PostList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="post-list">
+    <div class="post"
+         v-for="post in posts"
+         :key="post.id"
+    >
+
+      <div class="user-info">
+        <a href="#" class="user-name">{{userById(post.userId).name}}</a>
+
+        <a href="#">
+          <img :src="userById(post.userId).avatar" alt="" class="avatar-large">
+        </a>
+
+        <p class="desktop-only text-small">107 posts</p>
+      </div>
+
+      <div class="post-content">
+        <div>
+          <p>
+            {{post.text}}
+          </p>
+        </div>
+      </div>
+
+      <div class="post-date text-faded">
+        {{post.publishedAt}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue'
+import sourceData from '@/data.json'
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  posts: {
+    required: true,
+    type: Array
+  }
+})
+
+const users = ref(sourceData.users)
+
+function userById (userId) {
+  return users.value.find(u => u.id === userId)
+}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/pages/ThreadShow.vue
+++ src/components/pages/ThreadShow.vue
@@ -2,48 +2,18 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
 
-    <div class="post-list">
-      <div class="post"
-           v-for="postId in thread.posts"
-           :key="postId"
-      >
+    <post-list :posts="threadPosts" />
 
-        <div class="user-info">
-          <a href="#" class="user-name">{{userById(postById(postId).userId).name}}</a>
-
-          <a href="#">
-            <img :src="userById(postById(postId).userId).avatar" alt="" class="avatar-large">
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{posts.find(p => p.id === postId).text}}
-            </p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{postById(postId).publishedAt}}
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
 <script setup>
 import sourceData from '@/data.json'
+import PostList from '@/components/PostList.vue'
 import { computed, ref, defineProps } from 'vue'
-// import { useRoute } from 'vue-router'
-
-// const route = useRoute()
 
 const threads = ref(sourceData.threads)
 const posts = ref(sourceData.posts)
-const users = ref(sourceData.users)
 
 const props = defineProps({
   id: {
@@ -56,13 +26,10 @@ const thread = computed(() => {
   return threads.value.find(thread => thread.id === props.id)
 })
 
-function postById (postId) {
-  return posts.value.find(p => p.id === postId)
-}
+const threadPosts = computed(() => {
+  return posts.value.filter(post => post.threadId === props.id)
+})
 
-function userById (userId) {
-  return users.value.find(u => u.id === userId)
-}
 </script>
 
 <style scoped>
